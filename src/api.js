12aa541f8fe12d2dae8c7636ed5fe3a6a57fc8a0
roxx3r <roxx3r@st.lk>
2022-01-13 15:12:44 -0600

    use ap0calyp adapter code

--- src/api.js
+++ src/api.js
@@ -8,69 +8,21 @@ const cryptostats = new CryptoStatsSDK();
 const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY
 const MERLIN_ADDRESS = '0xb2c3a9c577068479b1e5119f6b7da98d25ba48f4'
 const MIM_ADDRESS = '0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3'
-const NETWORKS = {
-    ethereum: {
-        subgraph: 'ap0calyp/abracadabra-mainnet-fees',
-        blockchain: 'Ethereum',
-        protocolLaunch: '2021-05-27',
-    },
-    fantom: {
-        subgraph: 'ap0calyp/abracadabra-fantom-fees',
-        blockchain: 'Fantom',
-        protocolLaunch: '2021-05-27',
-    },
-    avalanche: {
-        subgraph: 'ap0calyp/abracadabra-avalanche-fees',
-        blockchain: 'Avalanche',
-        protocolLaunch: '2021-09-06',
-    },
-    bsc: {
-        subgraph: 'ap0calyp/abracadabra-binancesmartchain-fees',
-        blockchain: 'Binance',
-        protocolLaunch: '2021-11-17',
-    },
-    'arbitrum-one': {
-        subgraph: 'ap0calyp/abracadabra-arbitrum-fees',
-        blockchain: 'Arbitrum One',
-        protocolLaunch: '2021-09-15',
-    }
-}
-const FEE_QUERY = `query fees($startBlock: Int!) {
-  startValue: cauldronFees(block: { number: $startBlock }) {
-    accrueInfoFeesEarned
-    accrueInfoFeesWithdrawn
-  }
-  endValue: cauldronFees(limit: 1000) {
-    accrueInfoFeesEarned
-    accrueInfoFeesWithdrawn
-  }
-}`
+const IPFS_CID_ABRACADABRA_ADAPTER = 'QmV3vkgMJ12FXjCHjHABrUQ6kRpUzZ8WwGmzEbCGv7VxgY'
 
 module.exports.getFees = async (startDate) => {
-  // query all networks
-  const promiseArr = []
+  // fetch adapter
+  const feesList = cryptostats.getList('fees')
+  await feesList.fetchAdapterFromIPFS(IPFS_CID_ABRACADABRA_ADAPTER)
 
-  for (const network in NETWORKS) {
-    const { subgraph } = NETWORKS[network]
-    const blockNumber = await cryptostats.chainData.getBlockNumber(startDate, network)
-    const params = { variables: { startBlock: blockNumber } }
-    const promise = cryptostats.graph.query(subgraph, FEE_QUERY, params)
-    promiseArr.push(promise)
-  }
+  // query fees
+  const endDate = new Date()
+  const endDateFuture = endDate.getMinutes() - 10
+  endDate.setMinutes(endDateFuture)
+  const feeArr = await feesList.executeQuery('dateRangeTotalFees', startDate, endDate)
 
-  // aggregate fees
-  let totalFees = 0;
-  const networkQueries = await Promise.all(promiseArr)
-
-  networkQueries.forEach(({ startValue, endValue }) => {
-    const startFees = startValue.reduce((prev, curr) =>
-        prev + Number(curr.accrueInfoFeesEarned) + Number(curr.accrueInfoFeesWithdrawn), 0)
-    const endFees = endValue.reduce((prev, curr) =>
-        prev + Number(curr.accrueInfoFeesEarned) + Number(curr.accrueInfoFeesWithdrawn), 0)
-    const fees = endFees - startFees
-
-    totalFees += fees;
-  })
+  // reduce fees
+  const totalFees = feeArr.reduce((prev, { result }) => prev + result, 0)
 
   return totalFees
 }
