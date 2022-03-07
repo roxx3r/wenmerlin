bdad369447c34373284c2f3219a027194f2653d6
roxx3r <roxx3r@st.lk>
2022-03-07 17:22:12 -0600

    use try catch and add mim supply

--- src/lambda-fees.js
+++ src/lambda-fees.js
@@ -7,13 +7,18 @@ const db = require('./db')
 const cryptostats = new CryptoStatsSDK()
 
 // consatnts
-const IPFS_CID_ABRACADABRA_ADAPTER = 'QmV3vkgMJ12FXjCHjHABrUQ6kRpUzZ8WwGmzEbCGv7VxgY'
+const IPFS_CID_ABRACADABRA_ADAPTER = 'QmepmLhtmeFhZcEW2HhCAUg2QwRWbAs9rS3icbXB9JFbPU'
 const MERLIN_ADDRESS = '0xb2c3a9c577068479b1e5119f6b7da98d25ba48f4'
 
 module.exports.handler = async () => {
   // fetch adapter
   const feesList = cryptostats.getList('fees')
-  await feesList.fetchAdapterFromIPFS(IPFS_CID_ABRACADABRA_ADAPTER)
+  try {
+    await feesList.fetchAdapterFromIPFS(IPFS_CID_ABRACADABRA_ADAPTER)
+  } catch (e) {
+    console.error('fetchAdapterFromIPFS', e)
+  }
+
 
   // get last buybacks
   const { result } = await api.getTokenTxs()
@@ -29,19 +34,32 @@ module.exports.handler = async () => {
 
   // get mim supply at last buyback time
   const { blockNumber, timeStamp } = buybacks.slice(-1)[0]
-  const mimSupply = await api.getTokenSupply(blockNumber)
+  const { result: mimSupply } = await api.getTokenSupply(blockNumber)
   const mimSupplyE18 = Math.round(mimSupply / 1e18)
 
   // get fees since buyback
   const startDate = new Date(1000 * timeStamp - 1000)
   const endDate = new Date()
   endDate.setMinutes(endDate.getMinutes() - 10)
-  const feeArr = await feesList.executeQuery('dateRangeProtocolFees', startDate, endDate)
+  let feeArr
+
+  try {
+    feeArr = await feesList.executeQuery('dateRangeProtocolFees', startDate, endDate)
+  } catch (e) {
+    console.error('dateRangeProtocolFees', e)
+    try {
+      feeArr = await feesList.executeQuery('dateRangeTotalFees', startDate, endDate)
+    } catch (e) {
+      console.error('dateRangeTotalFees', e)
+      return
+    }
+  }
+
   const totalFees = feeArr.reduce((prev, { result }) => prev + result, 0)
 
   // add latest fees
-  const feesAndSupply = totalFees + mimSupplyE18
-  feeRecords.push({ ts: 9999999999, fees: Math.round(totalFees) })
+  const feesAndSupply = (totalFees + mimSupplyE18) * 0.75
+  feeRecords.push({ ts: 9999999999, fees: Math.round(feesAndSupply) })
 
   // store buybacks
   await db.batchWrite(feeRecords)
